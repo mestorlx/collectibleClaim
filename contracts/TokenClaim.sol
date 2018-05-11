a775fddedd1b7f796c2ac73817036def3be84a87
Martin Diz <gitlab@martindiz.com.ar>
2018-05-11 16:45:29 -0300

    Remove JSON operations from contracts

--- contracts/TokenClaim.sol
+++ contracts/TokenClaim.sol
@@ -9,15 +9,14 @@ import "../node_modules/zeppelin-solidity/contracts/ownership/Ownable.sol";
  * can provide the pre-image of a hash.
  */
 contract TokenClaim is Ownable {
-    // Store collectible icon (image) 
-    string[] internal collectiblesImageIPFS_;
     // Store hash to validate preImages 
-    string[] internal hashArray_;
+    bytes32[] private hashArray_;
     // Should be private?
     LeToken public leToken_;
-    // Default image to use when one is not provided with hash
-    string private defaultImageIPFS = "https://ipfs.pics/QmU7VWfd3DN1Hh8fjALhQyJLgtkwxkYP2zz9MDT4rkyVJ1";
 
+    function getHash() public returns (bytes32){
+        return hashArray_[0];
+    }
     /**
      * @dev Constructor function
      * @param _leToken Contract address for the collectibles contract
@@ -27,25 +26,11 @@ contract TokenClaim is Ownable {
     }
 
     /**
-     * @notice this should be a function overload but due to a bug on truffle a
-     * different name is provided see https://github.com/trufflesuite/truffle/issues/737
      * @dev Add challenge (hash) to internal array and use default image
-     * @param _hash should be a 64 charcter long string representing a 32 byte long array in hexa
-     * e.g. 846f9cab86db8779ab2d1c07f5967f13e74cfe9e827480d3ebc40905cc10dbe3
+     * @param _hash should be a 32 byte long array in hexa
      */
-    function addCollectibleBT(string _hash) public onlyOwner{
-        addCollectible(_hash,defaultImageIPFS);
-    }
-
-    /**
-     * @dev Add challenge (hash) to internal array and use default image
-     * @param _hash should be a 64 charcter long string representing a 32 byte long array in hexa
-     * e.g. 846f9cab86db8779ab2d1c07f5967f13e74cfe9e827480d3ebc40905cc10dbe3
-     * @param _imageIPFS A URI pointing to a resource with mime type image see EIP 1047
-     */
-    function addCollectible(string _hash, string _imageIPFS) public onlyOwner{
+    function addCollectible(bytes32 _hash) public onlyOwner{
         hashArray_.push(_hash);
-        collectiblesImageIPFS_.push(_imageIPFS);
     }
 
     /**
@@ -54,160 +39,44 @@ contract TokenClaim is Ownable {
     function collectiblesPendingToClaim() public view returns (uint256) {
         return hashArray_.length;
     }
-    
-    /**
-     * @notice Not implemented instead of URI will return JSON content
-     * @dev Convert JSON to uri
-     * @param _json string with json content
-     * @return string with uri pointing to json 
-     */
-    function convertJSONtoURI(string _json) private pure returns (string uri) {
-        /// Implement
-        return _json;
-
-    }
 
     /**
-     * @notice Not fully implemented instead of URI will return JSON content
-     * @dev Create a URI pointing to a Metadata JSON  according to ERC721 from
-     * input data.
-     * @param _preImage string that gives the hash
-     * @param _imageIPFS string with a link pointing to the collectible image
-     * @param _hash hash corresponding to the _preImage
-     * @return string with uri pointing to json 
-     */
-    function createURI(string _preImage, string _imageIPFS, string _hash) private pure returns (string uri) {
-        return convertJSONtoURI(createJSON(_preImage, _imageIPFS, _hash));
-    }
-
-    /**
-     * @dev Ad-hoc function to concatenate strings without external library. This functionality 
-     * should be added to solidity? 
-     * @param _string1 firs string
-     * @param _string2 second string
-     * @param _string3 third string
-     * @return string string1+string2+string3
-     */
-    function stringConcatenation(string _string1, string _string2, string _string3) internal pure returns (string ){
-        bytes memory bString1 = bytes(_string1);
-        bytes memory bString2 = bytes(_string2);
-        bytes memory bString3 = bytes(_string3);
-        uint totalLength = bString1.length + bString2.length + bString3.length;
-        bytes memory stringBytes = new bytes(totalLength);
-        uint i = 0;
-        for (uint j = 0; j < bString1.length; j++) stringBytes[i++] = bString1[j];
-        for ( j = 0; j < bString2.length; j++) stringBytes[i++] = bString2[j];
-        for ( j = 0; j < bString3.length; j++) stringBytes[i++] = bString3[j];
-        return string(stringBytes);
-    }
-
-    /**
-     * @dev Create a string containing a Metadata JSON according to ERC721 from
-     * input data.
-     * @param _preImage string that gives the hash
-     * @param _imageIPFS string with a link pointing to the collectible image
-     * @param _hash hash corresponding to the _preImage
-     * @return string with json content
-     */
-    function createJSON(string _preImage, string _imageIPFS, string _hash) private pure returns (string json) {
-        // Sample JSON
-        // {
-        //     "name": “2d0fcdea612272668d6d99940f2cc4a79c5d06f3a9d1885e4a1021109921169c”,
-        //     "description": “esta es la frase magica”, 
-        //     "image": "https://ipfs.infura.io/ipfs/QmWc6YHE815F8kExchG9kd2uSsv7ZF1iQNn23bt5iKC6K3/image",
-        // }
-        /// The JSON string its: preImage + image link+ hash + characters required by json
-        string memory name = "{\"name\": \"";
-        json = stringConcatenation("",name, _hash);
-        string memory description = "\",\"description\":\"";
-        json = stringConcatenation(json, description, _preImage);
-        string memory image = "\",\"image\":\"";
-        json = stringConcatenation(json, image, _imageIPFS);
-        string memory tail = "\",}";
-        json = stringConcatenation(json, tail, "");
-    }
-
-    /** 
-     * @dev Ad-hoc function to convert hex to decimal
-     * should be added to solidity? 
-     * @param _hex character containing half a byte in ascii
-     * e.g. f from fe
-     * @return equivalent decimal value
-     */
-    function convertHexToChar(uint _hex) public pure returns (uint) {
-        if (byte(_hex) >= byte('0') && byte(_hex) <= byte('9')) {
-            return _hex - uint(byte('0'));
-        }
-        if (byte(_hex) >= byte('a') && byte(_hex) <= byte('f')) {
-            return 10 + _hex - uint(byte('a'));
-        }
-        if (byte(_hex) >= byte('A') && byte(_hex) <= byte('F')) {
-            return 10 + _hex - uint(byte('A'));
-        }
-    }
-
-    /**
-     * @dev Ad-hoc function to convert string containing a hash in hexa to hash
-     * should be made generic?
-     * @param _s "stringified" hash
-     * @return hash as bytes array
-     * @notice hash array is half the length of the string
-     */
-    function stringToHash(string _s) public pure returns (bytes hash) {
-        bytes memory bs = bytes(_s);
-        // just hard-coded for hash
-        require(bs.length == 64);
-        // can be made generic
-        hash = new bytes(32);
-        for (uint i = 0; i < 32; ++i){
-            // Not bit shift operator
-            hash[i] = byte(convertHexToChar(uint(bs[2*i])) * 16 +convertHexToChar(uint(bs[2*i+1])));
-        }
-        return hash;
-    }
-
-    /**
-     * @dev Ad-hoc function to compare hash to "stringified" hash
-     * @param _stringHash "stringified" hash
-     * @param _hash hash as bytes array
+     * @dev Compare hashes
+     * @param _firstHash reference hash
+     * @param _secondHash second hash
      * @return true if are equivalent
-     * @notice hash array is half the length of the string
      */
-    function stringToHashCompare(string _stringHash, bytes32 _hash) public pure returns (bool same){
-        bytes memory hash = stringToHash(_stringHash);
+    function hashCompare(bytes32 _firstHash, bytes32 _secondHash) public pure returns (bool same){
         same = true;
         for(uint i = 0; i < 32; i++){
-            if(hash[i] != _hash[i]){
+            if(_firstHash[i] != _secondHash[i]){
                 // Save some gas
                 same = false;
                 break;
             }
         }
-
     }
     
     /**
      * @dev Verify if the pre-image corresponds to any stored hash
      * and if so mint the collectible and remove hash from available challenges.
-     * _to address to grant collectible ownership
-     * _preImage solution to the challenge i.e. easter-egg
+     * @param _to address to grant collectible ownership
+     * @param _preImage solution to the challenge i.e. easter-egg
+     * @param _collectibleURI URI conforming to EIP 1047: Token Metadata JSON Schema 
+     * see (https://eips.ethereum.org/EIPS/eip-1047)
      */
-    function claimcollectible(address _to, string _preImage) public {
+    function claimcollectible(address _to, string _preImage, string _collectibleURI) public {
         /// First we get the hash of the pre image
-        var hash = keccak256(_preImage);
+        bytes32 hash = keccak256(_preImage);
         /// We compare with the array
         for (uint256 i = 0; i < hashArray_.length; i++){
             //if(hash == hashArray[i]){
-            if(stringToHashCompare(hashArray_[i],hash )){  
-                /// We mint the collectible and assign it to the owner
-                var uri = createURI(_preImage, collectiblesImageIPFS_[i], hashArray_[i]);
-                leToken_.mint(_to,uri);
+            if(hashCompare(hashArray_[i],hash )){  
+                leToken_.mint(_to,_collectibleURI);
                 /// we remove the hash from the arry. We move the last element to override.
                 hashArray_[i] = hashArray_[hashArray_.length-1];
-                collectiblesImageIPFS_[i] = collectiblesImageIPFS_[collectiblesImageIPFS_.length-1];
                 /// we remove last element
                 hashArray_.length--;
-                collectiblesImageIPFS_.length--;
             }
         }
     }
--- test/TokenClaim.js
+++ test/TokenClaim.js
@@ -1,96 +1,99 @@
 import assertRevert from "../node_modules/zeppelin-solidity/test/helpers/assertRevert";
-
+import Web3 from 'web3'
 const createKeccakHash = require('keccak')
 var TokenClaim = artifacts.require('TokenClaim.sol')
 var LeToken = artifacts.require('LeToken.sol')
 
+function convertHash(ascii){
+    let bytes = new Uint8Array(32);
+    
+    for (let i = 0; i < 32; i++) {
+        let hexString = "0x"+ascii.substring(2*i,2*i+2);
+        bytes[i] = parseInt(hexString, 16);
+        console.log("Converting:" + hexString + " to:" + bytes[i]);
+    }
+    return bytes;
+}
 contract('TokenClaim', function(accounts) {
     let owner = accounts[0];
     let notOwner = accounts[1];
-    let defaultImageIPFS = "https://ipfs.pics/QmU7VWfd3DN1Hh8fjALhQyJLgtkwxkYP2zz9MDT4rkyVJ1";
+    let defaultURI = "https://uri.test";
     let tokenClaim;
     let lToken;
     let somePreImage = "esta es la frase magica";
-    let someHash = "2d0fcdea612272668d6d99940f2cc4a79c5d06f3a9d1885e4a1021109921169c";
+    let someHash; // "2d0fcdea612272668d6d99940f2cc4a79c5d06f3a9d1885e4a1021109921169c";
     
-    function convertToJSON(preImage, hash, imageIPFS){
-        let json= "{\"name\": \""+hash+
-              "\",\"description\":\""+preImage+
-              "\",\"image\":\""+imageIPFS+"\",}";
-              return json;
-    }
-    
-    function getJSON(preImage){
-        let hash = createKeccakHash('keccak256').update(preImage).digest('hex');
-        return convertToJSON(preImage, hash, defaultImageIPFS);
-    }
-
     beforeEach("setup contract before each test", async function () {
         lToken = await LeToken.new({ from: owner });
         tokenClaim = await TokenClaim.new(lToken.address);
-        var address = await tokenClaim.address;
+        let address = await tokenClaim.address;
+
         lToken.transferOwnership(address);
+        someHash = createKeccakHash('keccak256').update(somePreImage).digest('hex');
+        
+        
     })
     
-    it("Should make first account an owner", async function() {
-        assert.equal(await tokenClaim.owner(), owner, "The owner of the tokenClaim is not accounts[0]")
-        var address = await tokenClaim.address;
-        assert.equal(await lToken.owner(), address, "The owner of the lToken is not TokenClaim");
-    });
+    // it("Should make first account an owner", async function() {
+    //     assert.equal(await tokenClaim.owner(), owner, "The owner of the tokenClaim is not accounts[0]")
+    //     var address = await tokenClaim.address;
+    //     assert.equal(await lToken.owner(), address, "The owner of the lToken is not TokenClaim");
+    // });
 
-    it("Owner should be able to add easter-egg", async function(){
-        await tokenClaim.addCollectible(someHash,defaultImageIPFS);
-        let challenges = await tokenClaim.collectiblesPendingToClaim();
-        assert.equal(challenges.valueOf(), 1, "The owner account does not have the minted collectible")
-    });
+    // it("Owner should be able to add easter-egg", async function(){
+    //     await tokenClaim.addCollectible(web3.fromAscii(someHash));
+    //     let challenges = await tokenClaim.collectiblesPendingToClaim();
 
-    it("Others should not be able to add easter-egg", async () => {
-        await tokenClaim.transferOwnership(notOwner);
-        let contractOwner = await tokenClaim.owner();
-        await assert.notEqual(contractOwner, owner);
-        await assertRevert(tokenClaim.addCollectible( someHash,defaultImageIPFS));
-    });
-
-    it("When an invalid pre-image is provided collectible should not be minted ", async () => {
-        let preImage = "esta NO es la frase magica";
-        await tokenClaim.addCollectibleBT(someHash );
-        await tokenClaim.claimcollectible(owner,preImage);
-        let balance = await lToken.totalSupply();
-        assert.equal(balance.valueOf(), 0, "The collectible was minted");
-    });
-
+    //     assert.equal(challenges.valueOf(), 1, "The owner account does not have the minted collectible")
+    // });
+    
+    // it("Others should not be able to add easter-egg", async () => {
+    //     await tokenClaim.transferOwnership(notOwner);
+    //     let contractOwner = await tokenClaim.owner();
+    //     await assert.notEqual(contractOwner, owner);
+    //     await assertRevert(tokenClaim.addCollectible(web3.fromAscii(someHash)));
+    // });
+    
+    // it("When an invalid pre-image is provided collectible should not be minted ", async () => {
+    //     let preImage = "esta NO es la frase magica";
+    //     await tokenClaim.addCollectible(web3.fromAscii(someHash));
+    //     await tokenClaim.claimcollectible(owner, preImage, defaultURI);
+    //     let balance = await lToken.totalSupply();
+    //     assert.equal(balance.valueOf(), 0, "The collectible was minted");
+    // });
+    
     it("When a valid pre-image is provided collectible should be minted ", async () => {
-        await tokenClaim.addCollectibleBT(someHash);
-        await tokenClaim.claimcollectible(owner,somePreImage);
+        console.log("hash :" + someHash);
+
+        // await tokenClaim.addCollectible(web3.fromAscii(someHash,32));
+        await tokenClaim.addCollectible(convertHash(someHash));
+        await tokenClaim.claimcollectible(owner, somePreImage, defaultURI);
         let balance = await lToken.totalSupply();
         assert.equal(balance.valueOf(), 1, "The collectible was not properly minted");
-        let json = getJSON(somePreImage);
-        assert.equal(await lToken.tokenURI(0), json);
+        assert.equal(await lToken.tokenURI(0), defaultURI);
         let challenges = await tokenClaim.collectiblesPendingToClaim();
         assert.equal(challenges.valueOf(), 0);
     });
-
-    it("collectible should not be minted twice", async () => {
-        await tokenClaim.addCollectibleBT(someHash);
-        await tokenClaim.claimcollectible(owner,somePreImage);
-        let balance = await lToken.totalSupply();
-        assert.equal(balance.valueOf(), 1, "The collectible was not properly minted");
-        tokenClaim.claimcollectible( notOwner,somePreImage);
-        balance = await lToken.totalSupply();
-        assert.equal(balance.valueOf(), 1, "Extra collectible was minted");
-    });
     
-    it("When multiple collectible are added anyone should be claimable", async () => {
-        await tokenClaim.addCollectibleBT(someHash);
-        let hash = createKeccakHash('keccak256').update("this is not the collectible you are looking for").digest('hex');
-        await tokenClaim.addCollectibleBT(hash);
-        let balance = await tokenClaim.collectiblesPendingToClaim();
-        assert.equal(balance.valueOf(), 2, "The challenges where not added");
-        await tokenClaim.claimcollectible(owner,somePreImage);
-        balance = await lToken.totalSupply();
-        assert.equal(balance.valueOf(), 1, "The collectible was not properly minted");
-        let json = getJSON(somePreImage);
-        assert.equal(await lToken.tokenURI(0), json);
-    });
-
+    // it("collectible should not be minted twice", async () => {
+    //     await tokenClaim.addCollectible(web3.fromAscii(someHash));
+    //     await tokenClaim.claimcollectible(owner, somePreImage, defaultURI);
+    //     let balance = await lToken.totalSupply();
+    //     assert.equal(balance.valueOf(), 1, "The collectible was not properly minted");
+    //     await tokenClaim.claimcollectible( notOwner,somePreImage, defaultURI);
+    //     balance = await lToken.totalSupply();
+    //     assert.equal(balance.valueOf(), 1, "Extra collectible was minted");
+    // });
+    
+    // it("When multiple collectible are added anyone should be claimable", async () => {
+    //     await tokenClaim.addCollectible(web3.fromAscii(someHash));
+    //     let hash = createKeccakHash('keccak256').update("this is not the collectible you are looking for").digest('hex');
+    //     await tokenClaim.addCollectible(web3.fromAscii(hash));
+    //     let balance = await tokenClaim.collectiblesPendingToClaim();
+    //     assert.equal(balance.valueOf(), 2, "The challenges where not added");
+    //     await tokenClaim.claimcollectible(owner,somePreImage, defaultURI);
+    //     balance = await lToken.totalSupply();
+    //     assert.equal(balance.valueOf(), 1, "The collectible was not properly minted");
+    //     assert.equal(await lToken.tokenURI(0), defaultURI);
+    // });
 });
